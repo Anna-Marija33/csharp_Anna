68f5bb29d649b2e65ccfb666cb26561601ae6c00
anna <anna@ANNA>
2017-03-08 17:03:12 +0700

    Задание №4: Сделать всё то же самое, но для контактов

---
+++ addressbook-web-tests/addressbook-web-tests/ContactData.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebAddressbookTests
+{
+    class ContactData
+    {
+        private string finame;
+       
+
+        public ContactData(string finame)
+        {
+            this.finame = finame;
+        }
+
+
+        public string Finame
+        {
+            get
+            {
+                return finame;
+            }
+            set
+            {
+                finame = value;
+            }
+        }
+
+       
+    }
+}
