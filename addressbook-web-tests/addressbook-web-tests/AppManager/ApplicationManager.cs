f67cf38ebaf7eb59660b4e6e1758bdae94ac4562
anna <anna@ANNA>
2017-03-15 17:40:46 +0700

    Создан Хелпер для контакта. Начала создавать Тест по созданию контактаю То есть переписывать его в новом формате.

--- addressbook-web-tests/addressbook-web-tests/AppManager/ApplicationManager.cs
+++ addressbook-web-tests/addressbook-web-tests/AppManager/ApplicationManager.cs
@@ -3,38 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Firefox;
 using OpenQA.Selenium.Support.UI;
 
+
 namespace WebAddressbookTests
 {
-    public class TestBase
+    public class ApplicationManager
     {
         protected IWebDriver driver;
-        private StringBuilder verificationErrors;
         protected string baseURL;
 
         protected LoginHelper loginHelper;
         protected NavigationHelper navigator;
         protected GroupHelper groupHelper;
+        protected ContactHelper contactHelper;
 
-        [SetUp]
-        public void SetupTest()
+        public ApplicationManager()
         {
-            driver = new FirefoxDriver(new FirefoxBinary("C:\\Program Files\\Mozilla Firefox\\firefox.exe"), new FirefoxProfile());
-            baseURL = "http://localhost";
-            verificationErrors = new StringBuilder();
-
             loginHelper = new LoginHelper(driver);
             navigator = new NavigationHelper(driver, baseURL);
             groupHelper = new GroupHelper(driver);
+            contactHelper = new ContactHelper(driver);
         }
 
-        [TearDown]
-        public void TeardownTest()
+        public void Stop()
         {
+
             try
             {
                 driver.Quit();
@@ -43,10 +39,41 @@ namespace WebAddressbookTests
             {
                 // Ignore errors if unable to close the browser
             }
-            Assert.AreEqual("", verificationErrors.ToString());
+            
         }
 
-   
-       
+        public LoginHelper Auth
+        {
+            get
+            {
+                return loginHelper;
+            }
+        }
+
+        public NavigationHelper Navigator
+        {
+            get
+            {
+                return navigator;
+            }
+        }
+
+        public GroupHelper Groups
+        {
+            get
+            {
+                return groupHelper;
+            }
+        }
+
+        public GroupHelper Contacts
+        {
+            get
+            {
+                return contactHelper;
+            }
+        }
+
+
     }
 }
---
+++ addressbook-web-tests/addressbook-web-tests/ContactHelper.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+
+
+namespace WebAddressbookTests
+{
+    public class ContactHelper : HelperBase
+    {
+
+        public ContactHelper(IWebDriver driver) : base(driver)
+        {
+            this.driver = driver;
+        }
+
+        public void CreateNewContact()
+        {//Создание новго контакта
+            driver.FindElement(By.LinkText("add new")).Click();
+        }
+
+        public void ContactCreationTest(ContactData contact)
+        {//заполнение формы данными
+
+            driver.FindElement(By.Name("firstname")).Clear();
+            driver.FindElement(By.Name("firstname")).SendKeys(contact.Finame);
+        }
+
+      
+
+        private void SubmitContactCreation()
+        {//Подтверждение
+            driver.FindElement(By.Name("submit")).Click();
+            
+        }
+
+        
+        
+    }
+}
